bcd07c9cd59dac8dcba97425e1240b9a20d39d3b
Štěpán Beneš <benes.stepan@gmail.com>
2016-10-20 01:07:51 +0200

    Add solution to Problem 092. Add Common module

---
+++ ProjectEuler/Common.fs
@@ -0,0 +1,11 @@
+﻿module Common
+
+let rec parseDecimalDigits n =
+     seq {
+         if n > 0 then
+            let d = n % 10
+            yield d;
+            yield! parseDecimalDigits (n / 10)
+     }
+
+//let test = 987654321 |> parseDecimalDigits |> Seq.toList
---
+++ ProjectEuler/Problem092.fsx
@@ -0,0 +1,29 @@
+﻿// Square digit chains
+
+(*
+A number chain is created by continuously adding the square of the digits in a number to form a new number until it has been seen before.
+
+For example,
+
+44 → 32 → 13 → 10 → 1 → 1
+85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+
+Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop. What is most amazing is that EVERY starting number will eventually arrive at 1 or 89.
+
+How many starting numbers below ten million will arrive at 89?
+*)
+
+#load "Common.fs"
+
+open Common
+
+let sumSquaresOfDecimalDigits n = n |> parseDecimalDigits |> Seq.map (fun x -> x * x) |> Seq.sum
+
+let rec squareDigitChainEndsWith89 start =
+    let sum = sumSquaresOfDecimalDigits start
+    match sum with
+    | 1 -> false
+    | 89 -> true
+    | _ -> sum |> squareDigitChainEndsWith89
+
+let count = seq { 1 .. 9999999 } |> Seq.filter squareDigitChainEndsWith89 |> Seq.length

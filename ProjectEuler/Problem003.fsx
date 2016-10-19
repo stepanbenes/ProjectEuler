958c685ef446493fb4383e2385c9cb2119d20391
Štěpán Beneš <benes.stepan@gmail.com>
2016-10-19 18:53:40 +0200

    Baby steps (Solve 3 problems)

---
+++ ProjectEuler/Problem003.fsx
@@ -0,0 +1,34 @@
+﻿// Largest prime factor
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+let isPrime x =
+    if x < 2L then
+        false
+    else
+        let rec loop i d = 
+            match d with
+            | 1L -> true
+            | _ -> if i % d = 0L then false else loop i (d - 1L)
+        x |> float |> sqrt |> int64 |> loop x
+
+let rec nextPrime x =
+    match isPrime (x + 1L) with
+    | true -> x + 1L
+    | false -> nextPrime (x + 1L)
+
+let rec largestPrimeFactor x soFar =
+    let next = nextPrime soFar
+    match x % next with
+    | 0L -> 
+        let rest = x / next
+        match next < rest with
+        | true -> largestPrimeFactor rest next
+        | false -> next
+    | _ -> largestPrimeFactor x next
+
+let number = 600851475143L
+//let rootedNumber = number |> float |> sqrt |> int64        
+               
+let result = largestPrimeFactor number 1L

261a37f9216900b0b15ea593c8a3f23df18e7f75
Štěpán Beneš <benes.stepan@gmail.com>
2020-11-28 16:03:49 +0100

    Add solution to problem 8 in Rust

--- project_euler_rs/src/main.rs
+++ project_euler_rs/src/main.rs
@@ -3,6 +3,7 @@ mod problem_001;
 mod problem_002;
 mod problem_003;
 mod problem_004;
+mod problem_008;
 
 fn main() {
     println!("Problem 001: {}", problem_001::solve());
@@ -10,4 +11,5 @@ fn main() {
     println!("Problem 002 b: {}", problem_002::solve_iter());
     println!("Problem 003: {:?}", problem_003::solve());
     println!("Problem 004: {:?}", problem_004::solve());
+    println!("Problem 008: {}", problem_008::solve());
 }
